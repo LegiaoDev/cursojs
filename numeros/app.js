3afdf048f7b0dea53b57d4de02d74a49d8366dd1
Gabriel <gabrielantoniocp9@gmail.com>
2018-04-30 19:54:03 -0300

    Trabalhando com números e operações matemáticas

---
+++ numeros/app.js
@@ -0,0 +1,33 @@
+// Seção 2 Aula 8 = Numeros
+  // Variáveis
+    const numero1 = 100;
+    const numero2 = 50;
+    let valor;
+
+  // Operações Básicas
+
+    val = numero1 + numero2; // Soma
+    val = numero1 - numero2; // Subtração
+    val = numero1 / numero2; // Divisão
+    val = numero1 * numero2; // Multiplicação
+    val = numero1 % numero2; // Resto de Divisão
+
+  // Objeto matemático
+    val = Math.PI; // PI (Saída: 3.14)
+    val = Math.E; // Regra de Euler
+    val = Math.round(2.4); // Arredonda os valores (Saída: 2)
+    val = Math.ceil(2.4); // Arredonda para cima (Saída: 3)
+    val = Math.floor(2.4); // Arredonda para baixo (Saída: 2)
+    val = Math.sqrt(64); // Raíz quadrada (Saída: 8)
+    val = Math.abs(-3); // Valor Absoluto (Saída: 3)
+    val = Math.pow(8 , 2); // Potenciação (Saída: 64)
+    val = Math.min(2,3,4); // Pegar valor mais baixo (Saída: 2)
+    val = Math.max(1,2,3,7); // Retorna o maior valor (Saída: 7)
+    val = Math.random(); // Adiciona um decimal aleatório (Saída: 0.9721732197)
+    val = Math.random() * 20; // Adiciona um valor aleatório com decimal de 0 a 20 (Saída: 0.00001 > 19.99999)
+    val = Math.random() * 20 + 1 // Adiciona um valor aleatório que começa em 1 e termina em 20 (Saída: 1.00000 > 19.99999)
+    val = Math.floor(Math.random() * 20 + 1); // Valores inteiros entre 1 e 20 (Saída: 1 > 19)
+
+
+  // Saída
+    console.log(val);
